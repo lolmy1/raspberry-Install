ff7781b47f760020a7d14183633c4ab277078d8a
lolmy1 <83218047+lolmy1@users.noreply.github.com>
2022-03-11 12:19:23 +0100

    Update ftpuser.sh

--- ftpuser.sh
+++ ftpuser.sh
@@ -8,5 +8,6 @@ fi
 read -p "Wie soll der neue Benutzer heißen?" username
 read -p "Welches Passwort für den User?" userpw
 
+echo "/bin/false" | tee -a test.txt
 useradd -m -G www-data -s /bin/false $username
 echo $username:$userpw | chpasswd
