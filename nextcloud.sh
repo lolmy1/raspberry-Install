c47879d353977f9b4fb364d5905c6b8bbd9f6fd4
lolmy1 <83218047+lolmy1@users.noreply.github.com>
2022-03-12 09:46:59 +0100

    Update nextcloud.sh

--- nextcloud.sh
+++ nextcloud.sh
@@ -5,6 +5,8 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+ip=$(hostname -I)
+
 read -p "Wie soll der User der Datenbank heißen?" sqluser
 read -p "Wie soll das Passwort für $sqluser sein?" sqluserpw
 read -p "Benutzername für den Adminaccount der Cloud?" adminname
@@ -13,16 +15,15 @@ read -p "Passwort für den Adminaccount?" adminpw
 apt update
 apt install php php-ctype php-curl php-dom php-gd php-json php-mbstring php-zip php-mysql php-bz2 php-intl php-ldap php-imap php-bcmath php-gmp php-imagick libapache2-mod-php7.4 apache2 mariadb-server -y
 
-mysql -uroot <<Query
-CREATE USER $sqluser@'localhost' IDENTIFIED BY '$sqluserpw';
-CREATE DATABASE IF NOT EXISTS nextcloud CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-GRANT ALL PRIVILEGES ON nextcloud.* TO 'username'@'localhost';
-FLUSH PRIVILEGES;
-Query
+mysql -uroot -e "CREATE USER $sqluser@'localhost' IDENTIFIED BY '${sqluserpw}'"
+mysql -uroot -e "CREATE DATABASE IF NOT EXISTS nextcloud CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON nextcloud.* TO $sqluser@'localhost'"
+mysql -uroot -e "FLUSH PRIVILEGES"
 
 wget -P /tmp/ https://download.nextcloud.com/server/releases/nextcloud-23.0.2.zip
 unzip /tmp/nextcloud-23.0.2.zip -d /var/www/
 chown www-data:www-data -Rv /var/www/nextcloud/
+chmod -Rv 755 /var/www/nextcloud/
 
 a2enmod rewrite
 a2enmod headers
@@ -45,7 +46,9 @@ echo "Alias /nextcloud "/var/www/nextcloud/"
 </Directory>" | tee /etc/apache2/sites-available/nextcloud.conf
 
 a2ensite nextcloud.conf
+sed -i 's/memory_limit = 128M/memory_limit = 512M/g' /etc/php/7.4/apache2/php.ini
 systemctl restart apache2
 
 cd /var/www/nextcloud/
+sudo -u www-data php occ config:system:set trusted_domains 1 --value=$ip
 sudo -u www-data php occ  maintenance:install --database mysql --database-name nextcloud  --database-user $sqluser --database-pass $sqluserpw --admin-user $adminname --admin-pass $adminpw
